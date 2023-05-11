d9bda36ae54e8d96be93a414167427fcd10e6638
Fabs <ossesvfabian@gmail.com>
2023-05-11 16:12:29 -0400

    ay me muelo

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, request, render_template, redirect, url_for, session
 UPLOAD_FOLDER = 'static/uploads'
 
 app = Flask(__name__)
-app.secret_key = "s3cr3t_k3y"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
