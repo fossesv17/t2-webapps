ce94b6fe02da813690dff0e65068a473794dfa14
Fabs <ossesvfabian@gmail.com>
2023-05-12 17:56:23 -0400

    avances hehe

--- app.py
+++ app.py
@@ -10,5 +10,30 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def home():
     return render_template('inicio.html')
 
+@app.route('/agregar-donacion')
+def add_donation():
+    return render_template('agregar-donacion.html')
+
+@app.route('/agregar-pedido')
+def add_order():
+    return render_template('agregar-pedido.html')
+
+@app.route('/ver-donaciones')
+def check_donations():
+    return render_template('ver-donaciones.html')
+
+@app.route('/ver-pedidos')
+def check_orders():
+    return render_template('ver-pedidos.html')
+
+@app.route('/informacion-donacion')
+def donation_info():
+    return render_template('informacion-donacion.html')
+
+@app.route('/informacion-pedido')
+def order_info():
+    return render_template('informacion-pedido.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
